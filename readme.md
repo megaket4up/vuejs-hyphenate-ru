6585391a5f3626592eed5b86ac475b884dd7ec4f
Askar <a.allagulov@krus-com.ru>
2017-12-28 15:01:56 +0500

    Init

---
+++ readme.md
@@ -0,0 +1,4 @@
+# vuejs-ru-hyphenate
+Плагин для расстановки переносов в русском тексте при выправке по формату (text-align: justify)
+
+Источник алгоритма расстановки переносов: http://xpoint.ru/know-how/VebAlgoritmyi/RabotaSTekstami/RasstanovkaPerenosov
---
+++ src/hyphenateRussianPlugin.js
@@ -0,0 +1,42 @@
+export function hyphenate(text) {
+    let RusA = "[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]";
+    let RusV = "[аеёиоуыэю\я]";
+    let RusN = "[бвгджзклмнпрстфхцчшщ]";
+    let RusX = "[йъь]";
+    let Hyphen = "\xAD";
+
+    let re1 = new RegExp("("+RusX+")("+RusA+RusA+")","ig");
+    let re2 = new RegExp("("+RusV+")("+RusV+RusA+")","ig");
+    let re3 = new RegExp("("+RusV+RusN+")("+RusN+RusV+")","ig");
+    let re4 = new RegExp("("+RusN+RusV+")("+RusN+RusV+")","ig");
+    let re5 = new RegExp("("+RusV+RusN+")("+RusN+RusN+RusV+")","ig");
+    let re6 = new RegExp("("+RusV+RusN+RusN+")("+RusN+RusN+RusV+")","ig");
+
+    text = text.replace(re1, "$1"+Hyphen+"$2");
+    text = text.replace(re2, "$1"+Hyphen+"$2");
+    text = text.replace(re3, "$1"+Hyphen+"$2");
+    text = text.replace(re4, "$1"+Hyphen+"$2");
+    text = text.replace(re5, "$1"+Hyphen+"$2");
+    text = text.replace(re6, "$1"+Hyphen+"$2");
+
+    return text;
+}
+
+export const hyphenateDirective = {
+    inserted(el) {
+        el.innerText = hyphenate(el.innerText);
+    }
+};
+
+const myselectPlugin = {
+    install: function (Vue, options) {
+        Vue.directive('hyphenate', directive);
+    },
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(myselectPlugin);
+}
+
+export default myselectPlugin;
